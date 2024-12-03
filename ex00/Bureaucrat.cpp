21832639e1a4db21df90f22cf36c91de6e43143f
Cyprien Ferreira <cyferrei@paul-f4Br5s5.clusters.42paris.fr>
2024-12-03 16:42:15 +0100

    [ADD]

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -6,18 +6,21 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 15:37:05 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/11/28 16:02:02 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/12/03 16:38:02 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-
 Bureaucrat::Bureaucrat() : _name("default"), _grade(1) {
 	std::cout << "Bureaucrat default constructor called" << std::endl;
 }
 
-Bureaucrat::Bureaucrat(const std::string name, unsigned int grade) : _name(name), _grade(grade) {
+Bureaucrat::Bureaucrat(const std::string &name, int grade) : _name(name), _grade(grade) {
+	if (grade < MAX_GRADE)
+		throw GradeTooHighException();
+	else if (grade > LOW_GRADE)
+		throw GradeTooLowException();
 	std::cout << "Bureaucrat custom constructor called" << std::endl;
 }
 
@@ -28,7 +31,7 @@ Bureaucrat::Bureaucrat(const Bureaucrat & other) {
 
 Bureaucrat & Bureaucrat::operator=(const Bureaucrat & other) {
 	if (this != &other) {
-		this->_grade = getGrade();
+		this->_grade = other.getGrade();
 	}
 	return *this;
 }
@@ -37,10 +40,57 @@ Bureaucrat::~Bureaucrat() {
 	std::cout << "Bureaucrat destructor called" << std::endl;
 }
 
+/*********************************************************************************************************/
+
 const std::string &Bureaucrat::getName(void)const {
 	return (this->_name);
 }
 
-unsigned int Bureaucrat::getGrade(void) {
+unsigned int Bureaucrat::getGrade(void) const {
 	return (this->_grade);
+}
+
+/*********************************************************************************************************/
+
+void Bureaucrat::incrementGrade(int value) {
+	int old_grade = this->getGrade();
+	
+	if (old_grade + value > LOW_GRADE) {
+		throw GradeTooLowException();
+	}
+	else {
+		this->_grade++;
+		std::cout << "You've been downgraded!" << std::endl;
+	}	
+}
+
+void Bureaucrat::decrementGrade(int value) {
+	int old_grade = this->getGrade();
+	
+	if (old_grade - value < MAX_GRADE) {
+		throw GradeTooHighException();
+	}
+	else {
+		this->_grade--;
+		std::cout << "You lvlUP!" << std::endl;
+	}	
+}
+
+/*********************************************************************************************************/
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &b) {
+	out << "[NAME]: " << b.getName() << std::endl;
+	out << "[GRADE]: " << b.getGrade() << std::endl;
+	
+	return out;
+}
+
+/*********************************************************************************************************/
+
+const char *Bureaucrat::GradeTooHighException::what()const throw() {
+	return ("Bureaucrat grade too hight!");
+}
+
+const char *Bureaucrat::GradeTooLowException::what()const throw() {
+	return ("Bureaucrat grade too low!");
 }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,11 +6,58 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 15:37:11 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/11/28 15:40:44 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/12/03 16:40:50 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main (void) {
-	
-	return (0);
+#include "Bureaucrat.hpp"
+#include <iostream>
+
+int main() {
+	try {
+		// Test du constructeur par défaut
+		Bureaucrat defaultBureaucrat;
+		std::cout << defaultBureaucrat << std::endl;
+
+		// Test du constructeur personnalisé
+		Bureaucrat customBureaucrat("Alice", 1);
+		std::cout << customBureaucrat << std::endl;
+
+		// Test de la fonction decrementGrade()
+		std::cout << "Decrementing grade of Alice by 1:" << std::endl;
+		customBureaucrat.decrementGrade(1);
+		std::cout << customBureaucrat << std::endl;
+		
+		// Test de la fonction incrementGrade()
+		std::cout << "Incrementing grade of Alice by 1:" << std::endl;
+		customBureaucrat.incrementGrade(1);
+		std::cout << customBureaucrat << std::endl;
+
+
+		// Test d'une exception GradeTooHighException
+		std::cout << "Testing GradeTooHighException:" << std::endl;
+		Bureaucrat highBureaucrat("Bob", 0); // Devrait lever une exception
+		std::cout << highBureaucrat << std::endl;
+	} 
+	catch (const Bureaucrat::GradeTooHighException &e) {
+		std::cerr << e.what() << std::endl;
+	} 
+	catch (const Bureaucrat::GradeTooLowException &e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	try {
+		// Test d'une exception GradeTooLowException
+		std::cout << "Testing GradeTooLowException:" << std::endl;
+		Bureaucrat lowBureaucrat("Charlie", 160); // Devrait lever une exception
+		std::cout << lowBureaucrat << std::endl;
+	} 
+	catch (const Bureaucrat::GradeTooHighException &e) {
+		std::cerr << e.what() << std::endl;
+	} 
+	catch (const Bureaucrat::GradeTooLowException &e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	return 0;
 }
