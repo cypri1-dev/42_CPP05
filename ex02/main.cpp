a62b533ae0f79401b8962d1bcf6c3c2427165698
Cyprien Ferreira <cyferrei@paul-f4Br7s3.clusters.42paris.fr>
2024-12-09 16:07:16 +0100

    [MODIF]: modif in main fior tests

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,27 +6,44 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 15:32:20 by cyferrei          #+#    #+#             */
-/*   Updated: 2024/12/06 16:55:26 by cyferrei         ###   ########.fr       */
+/*   Updated: 2024/12/09 16:05:01 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "AForm.hpp"
+#include "Bureaucrat.hpp"
 #include "PresidentialPardonForm.hpp"
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
 
 int main (void) {
-	AForm test;
-	std::cout << test << std::endl << std::endl;
 
-	ShrubberyCreationForm s("Toto");
-	std::cout << s << std::endl;
+	try {
+		Bureaucrat b("Brakis", 1);
+		// PresidentialPardonForm p("Toto");
+		// RobotomyRequestForm r("Warrox");
+		ShrubberyCreationForm s("Robin");
 
-	RobotomyRequestForm r("Brakis");
-	std::cout << r << std::endl;
+		std::cout << b << std::endl;
+		// std::cout << p << std::endl;
+		std::cout << s << std::endl;
 
-	PresidentialPardonForm p("Cousto");
-	std::cout << p << std::endl;
-	
+
+		b.signForm(s);
+		s.execute(b);
+		
+		// ShrubberyCreationForm s("Toto");
+		// std::cout << s << std::endl;
+
+		// RobotomyRequestForm r("Brakis");
+		// std::cout << r << std::endl;
+
+		// PresidentialPardonForm p("Cousto");
+		// std::cout << p << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+
 	return (0);
 }
